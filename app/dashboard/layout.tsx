7029fac21a1b0b8042b8843aae9bb994b0d01d55
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-07 08:37:44 +0700

    implement dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./_components/Header";
+
+export default function DashboardLayout({ children }: any) {
+  return (
+    <div>
+      <Header />
+      <div className="mx-5 md:mx-20 lg:mx-36">{children}</div>
+    </div>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Dashboard({ children }: any) {
+  return (
+    <div className="p-10">
+      <h2 className="font-bold text-3xl text-primary">Dashboard</h2>
+      <h2 className="text-gray-500">Create and Start your AI Mockup Interview</h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 my-5 gap-5">{/* Add Interview  */}</div>
+
+      {/* Previous Interview List  */}
+    </div>
+  );
+}
